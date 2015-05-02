bea0e1db1e6fd2a32447501635901763460985ac
Nobody <w.spies@live.de>
2015-05-02 16:27:07 +0200

    add option scripts/styles in templateroot yes/no

--- ws-navigator-pack/mod_wsnavigator/mod_wsnavigator.php
+++ ws-navigator-pack/mod_wsnavigator/mod_wsnavigator.php
@@ -23,13 +23,18 @@ $showAll	= $params->get('showAllChildren');
 $class_sfx	= htmlspecialchars($params->get('class_sfx'));
 $doc        = JFactory::getDocument();
 $app        = JFactory::getApplication();
+$scToTmpl = $params->get('addscriptstotemplate');
+$scriptRoot = ($scToTmpl != null && $scToTmpl > 0)? '/templates/' . $app->getTemplate(): '/modules/mod_wsnavigator';
 
-$doc->addScript('/modules/mod_wsnavigator/js/snap.svg-min.js');
-$doc->addScript('/modules/mod_wsnavigator/js/ws-navigator.js');
+$doc->addScript($scriptRoot . '/js/snap.svg-min.js');
+$doc->addScript($scriptRoot . '/js/ws-navigator.js');
+// Add Stylesheets
+$doc->addStyleSheet($scriptRoot . '/css/navigation.css');
+
+//always in template
 $doc->addScript('/templates/' . $app->getTemplate() . '/js/navigation.js');
 
-// Add Stylesheets
-$doc->addStyleSheet('/modules/mod_wsnavigator/css/navigation.css');
+
 if (count($list))
 {
     require JModuleHelper::getLayoutPath('mod_wsnavigator', $params->get('layout', 'default'));
